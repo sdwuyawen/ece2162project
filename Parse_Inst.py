c57f53c0d7608ca1969170aa877053594a73adba
Kevin Jia <coldfish@KevindeMacBook-Pro-2.local>
2018-11-03 13:58:42 -0400

    instruction is converted into integer indication when reading the instructions set

--- Parse_Inst.py
+++ Parse_Inst.py
@@ -15,7 +15,9 @@ def parse_Inst():
         y = x.split(' ')
 
         if len(y) == 4:
-            print(y[0])
+
+            operand_construct = Inst_No(y[0])
+            print(operand_construct)
 
             print(y[1].split(',')[0])
             dest_operand_1 = int(y[1].split(',')[0][1:])
@@ -29,8 +31,8 @@ def parse_Inst():
             source_operand_1 = int(y[3].split(',')[0][1:])
             print(source_operand_1)
 
-            p = Instruction(y[0], dest_operand_1, source_operand_0, source_operand_1)
-            print ("p's inst is "+p.inst)
+            p = Instruction(operand_construct, dest_operand_1, source_operand_0, source_operand_1)
+            print ("p's inst is",p.inst)
 
         if len(y) == 3:
             print(y[0])
@@ -40,5 +42,28 @@ def parse_Inst():
             print(y[2].split('\n')[0])
     print("end!")
 
+def Inst_No (inst_type):
+    if inst_type == "Ld":
+        return 1
+    elif inst_type == "Sd":
+        return 2
+    elif inst_type == "Sd":
+        return 3
+    elif inst_type == "Beq":
+        return 4
+    elif inst_type == "Bne":
+        return 5
+    elif inst_type == "Add":
+        return 6
+    elif inst_type == "Add.d":
+        return 7
+    elif inst_type == "Addi":
+        return 8
+    elif inst_type == "Sub":
+        return 9
+    elif inst_type == "Sub.d":
+        return 10
+    elif inst_type == "Mult.d":
+        return 11
 
 parse_Inst()
